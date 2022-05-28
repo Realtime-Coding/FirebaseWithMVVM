7f2a16a582e0913f6eb99cc7e4f74f87540d088d
Shahzad Afridi <shahzad@tentwenty.me>
2022-05-28 14:17:34 +0500

    Firebase setup done

--- app/src/main/java/com/example/firebasewithmvvm/MainActivity.kt
+++ app/src/main/java/com/example/firebasewithmvvm/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.firebasewithmvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
