220a5e64e0dce820791d8f1acbe4660575707496
Shahzad Afridi <shahzad@tentwenty.me>
2022-05-29 13:03:35 +0500

    staggeredGridLayoutManager added to the reyclerview

--- app/src/main/java/com/example/firebasewithmvvm/note/NoteListingFragment.kt
+++ app/src/main/java/com/example/firebasewithmvvm/note/NoteListingFragment.kt
@@ -1,13 +1,14 @@
 package com.example.firebasewithmvvm.note
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.firebasewithmvvm.R
 import com.example.firebasewithmvvm.databinding.FragmentNoteListingBinding
 import com.example.firebasewithmvvm.util.UiState
@@ -15,6 +16,7 @@ import com.example.firebasewithmvvm.util.hide
 import com.example.firebasewithmvvm.util.show
 import com.example.firebasewithmvvm.util.toast
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class NoteListingFragment : Fragment() {
@@ -42,6 +44,8 @@ class NoteListingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val staggeredGridLayoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        binding.recyclerView.layoutManager = staggeredGridLayoutManager
         binding.recyclerView.adapter = adapter
         binding.button.setOnClickListener {
             findNavController().navigate(R.id.action_noteListingFragment_to_noteDetailFragment)
