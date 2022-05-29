1a064747df728a15baf0320ef50a4634b453a4c2
Shahzad Afridi <shahzad@tentwenty.me>
2022-05-29 13:48:57 +0500

    Modified chip extension fucntion with close icon

--- app/src/main/java/com/example/firebasewithmvvm/util/Extensions.kt
+++ app/src/main/java/com/example/firebasewithmvvm/util/Extensions.kt
@@ -1,9 +1,9 @@
 package com.example.firebasewithmvvm.util
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.firebasewithmvvm.R
 import com.google.android.material.chip.Chip
@@ -30,9 +30,19 @@ fun Fragment.toast(msg: String?){
     Toast.makeText(requireContext(),msg,Toast.LENGTH_LONG).show()
 }
 
-fun ChipGroup.addChip(text: String) {
+fun ChipGroup.addChip(
+    text: String,
+    isTouchTargeSize: Boolean = false,
+    closeIconListener: View.OnClickListener? = null
+) {
     val chip: Chip = LayoutInflater.from(context).inflate(R.layout.item_chip,null,false) as Chip
     chip.text = if (text.length > 9) text.substring(0,9) + "..." else text
     chip.isClickable = false
+    chip.setEnsureMinTouchTargetSize(isTouchTargeSize)
+    if (closeIconListener != null){
+        chip.closeIcon = ContextCompat.getDrawable(context, com.google.android.material.R.drawable.ic_mtrl_chip_close_circle)
+        chip.isCloseIconVisible = true
+        chip.setOnCloseIconClickListener(closeIconListener)
+    }
     addView(chip)
 }
