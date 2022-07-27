eef8caf4c7b17324a229c42cc894eca1b3372b28
Shahzad Afridi <shahzadahmadafridi@gmail.com>
2022-07-27 18:47:54 +0500

    Task listing added

--- app/src/main/java/com/example/firebasewithmvvm/data/repository/TaskRepository.kt
+++ app/src/main/java/com/example/firebasewithmvvm/data/repository/TaskRepository.kt
@@ -1,8 +1,10 @@
 package com.example.firebasewithmvvm.data.repository
 
 import com.example.firebasewithmvvm.data.model.Task
+import com.example.firebasewithmvvm.data.model.User
 import com.example.firebasewithmvvm.util.UiState
 
 interface TaskRepository {
     fun addTask(task: Task, result: (UiState<Pair<Task,String>>) -> Unit)
+    fun getTasks(user: User?, result: (UiState<List<Task>>) -> Unit)
 }
---
+++ app/src/main/java/com/example/firebasewithmvvm/ui/task/TaskListingAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.firebasewithmvvm.ui.task
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.firebasewithmvvm.data.model.Task
+import com.example.firebasewithmvvm.databinding.TaskLayoutBinding
+
+class TaskListingAdapter(
+    val onDeleteClicked: ((Int, Task) -> Unit)? = null,
+) : RecyclerView.Adapter<TaskListingAdapter.MyViewHolder>() {
+
+    private var list: MutableList<Task> = arrayListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = TaskLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = list[position]
+        holder.bind(item,position)
+    }
+
+    fun updateList(list: MutableList<Task>){
+        this.list = list
+        notifyDataSetChanged()
+    }
+
+    fun removeItem(position: Int){
+        list.removeAt(position)
+        notifyItemChanged(position)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    inner class MyViewHolder(val binding: TaskLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Task,position: Int) {
+            binding.title.setText(item.description)
+            binding.delete.setOnClickListener {
+                onDeleteClicked?.invoke(position,item)
+            }
+        }
+    }
+}
