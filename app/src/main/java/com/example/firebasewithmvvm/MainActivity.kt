5a67f537e80fb2ce05e1c72940319ee456247154
Shahzad Afridi <shahzadahmadafridi@gmail.com>
2022-10-02 23:40:07 +0500

    Fixed home back button to minimze app

--- app/src/main/java/com/example/firebasewithmvvm/MainActivity.kt
+++ app/src/main/java/com/example/firebasewithmvvm/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.firebasewithmvvm
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.NavGraph
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.firestore.DocumentReference
@@ -15,8 +17,21 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (navController.currentDestination?.id == R.id.loginFragment){
+            moveTaskToBack(true)
+        }else{
+            super.onBackPressed()
+        }
     }
 }
